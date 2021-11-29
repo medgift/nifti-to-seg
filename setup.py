db48ba76eb83757062c94cd2814677ac37d5bb4d
Roger Schaer <roger.schaer@hevs.ch>
2021-11-29 10:58:05 +0100

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nifti-to-seg",
+    version="1.0.0",
+    url="https://github.com/roger-schaer/nifti-to-seg.git",
+    author="Roger Schaer",
+    author_email="roger.schaer@hevs.ch",
+    description="This project allows you to convert a NIfTI file containing one or more non-overlapping regions-of-interest (ROIs) into the DICOM Segmentation (SEG) format",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "git+https://github.com/roger-schaer/pydicom-seg.git#egg=pydicom-seg",
+        "SimpleITK",
+        "palettable",
+    ],
+)
